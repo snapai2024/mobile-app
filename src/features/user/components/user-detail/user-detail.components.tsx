466ef4d475392c56414093f1dcc8498a29a3e938
ludovic.sobrero <sobrero.ludovic@gmail.com>
2024-01-25 11:14:43 +0100

    feat: adding update on user

--- src/features/user/components/user-detail/user-detail.components.tsx
+++ src/features/user/components/user-detail/user-detail.components.tsx
@@ -1,21 +1,52 @@
 import {
   IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
   IonInput,
   IonItem,
   IonList,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import { FC } from "react";
-import { useGetMeQuery } from "../../services/api";
+import { FC, useEffect, useState } from "react";
+import { useGetMeQuery, usePatchUserMutation } from "../../services/api";
 import { authenticationActions } from "../../../auth/services/auth.slice";
 import { useDispatch } from "react-redux";
+import { UserFormData, UserRequest } from "../../models/user";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { logOut } from "ionicons/icons";
 
 const UserDetail: FC = () => {
   const { data: user } = useGetMeQuery();
   const dispatch = useDispatch();
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [patchUser, { isSuccess }] = usePatchUserMutation();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserFormData>();
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success('Modifications enregistrées.')
+      setIsEditing(false);
+    }
+  }, [isSuccess])
+
+  const onSubmit = (d: UserFormData) => {
+    const { email, password } = d;
+
+    const data: UserRequest = {
+      email: d.email || undefined,
+      password: d.password || undefined,
+    };
+
+    patchUser({ id: user?.id, ...data});
+  }
 
   if (!user) return;
 
@@ -24,26 +55,45 @@ const UserDetail: FC = () => {
       <IonHeader>
         <IonToolbar>
           <IonTitle>Mon compte</IonTitle>
+          <IonButtons slot="end">
+              <IonButton strong={true} onClick={() => setIsEditing(!isEditing)}>
+                {!isEditing ? "Modifier" : 'Annuler'}
+              </IonButton>
+            </IonButtons>
         </IonToolbar>
       </IonHeader>
 
       <IonContent className="ion-padding">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <IonList>
             <IonItem>
-              <IonInput label="Adresse email" value={user.email} disabled />
+              <IonInput 
+              label="Adresse email" 
+              value={user.email} 
+              disabled={!isEditing} 
+              {...register('email', { disabled: !isEditing })}
+              />
             </IonItem>
             <IonItem>
               <IonInput
                 type="password"
                 label="Mot de passe"
-                value="abcde12345"
-                disabled
+                value={!isEditing ? "abcde12345" : ""}
+                disabled={!isEditing}
+                {...register('password', { disabled: !isEditing })}
               />
             </IonItem>
+            {
+              isEditing && (
+                <IonButton size="small" type="submit" className="ion-margin-top" style={{ width: '100%' }}>
+                  Enregistrer
+                </IonButton>
+              )
+            }
           </IonList>
         </form>
-        <IonButton className="ion-margin-top" style={{ width: '100%' }} onClick={() => dispatch(authenticationActions.logout())}>
+        <IonButton style={{ width: '70%', position: 'fixed', bottom: '10px', left: 0, right: 0, margin: '0 auto' }} onClick={() => dispatch(authenticationActions.logout())}>
+          <IonIcon slot="start" icon={logOut}></IonIcon>
           Se déconnecter
         </IonButton>
       </IonContent>
